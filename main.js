4cf5b78e1315c31869370613f2a023cbcea96e22
Zhanasyl08 <102581548+Zhanasyl08@users.noreply.github.com>
2026-04-03 16:59:06 +0500

    first commit

--- main.js
+++ main.js
@@ -1,21 +1,19 @@
-export default {
-  name: "App",
-  data() {
-    return {
-      menuOpen: false,
-    };
-  },
-};
-r = document.getElementById("burger");
+const burger = document.getElementById("burger");
 const menu = document.getElementById("menu");
+const closeBtn = document.getElementById("closebutton");
 const overlay = document.getElementById("overlay");
 
 burger.addEventListener("click", () => {
-    menu.classList.toggle("active");
-    overlay.classList.toggle("active");
+    menu.classList.add("active");
+    overlay.style.display = "block";
+});
+
+closeBtn.addEventListener("click", () => {
+    menu.classList.remove("active");
+    overlay.style.display = "none";
 });
 
 overlay.addEventListener("click", () => {
     menu.classList.remove("active");
-    overlay.classList.remove("active");
+    overlay.style.display = "none";
 });
